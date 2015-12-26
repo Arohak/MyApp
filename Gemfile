496e1ce740e7ed9f12c8a56f5fa4e568dbde755c
arohak <aro.hak.25@gmail.com>
2015-12-26 21:29:50 +0000

    add like/dislike functionlity, add pagination

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'carrierwave'
 gem "mini_magick"
+gem "will_paginate"
+gem "bootstrap-will_paginate"
 
 group :development, :test do
   gem 'sqlite3', '~> 1.3.11'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
-  
   def index
-    @recipes = Recipe.all  
+    # @recipes = Recipe.all.sort_by{|like| like.thumbs_up_total}.reverse
+    @recipes = Recipe.paginate(page: params[:page], per_page: 3)
   end
   
   def show
@@ -15,7 +15,6 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.chef = Chef.find(1)
-    
     if @recipe.save 
       flash[:success] = "Your recipe was created succesfully"
       redirect_to recipes_path
@@ -30,19 +29,29 @@ class RecipesController < ApplicationController
   
   def update
     @recipe = Recipe.find(params[:id])
-
     if @recipe.update(recipe_params) 
-      flash[:success] = "Your recipe updateed succesfully"
+      flash[:success] = "Your recipe updated succesfully"
       redirect_to recipe_path(@recipe)
     else
       render :edit
     end
   end
   
-  private 
-  
-  def recipe_params
-    params.require(:recipe).permit(:name, :summary, :description, :picture)
+  def like 
+  @recipe = Recipe.find(params[:id])
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection send succesfully"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like/dislike a recipe once"
+      redirect_to :back  
+    end
+
   end
-    
+  
+  private 
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description, :picture)
+    end
 end
